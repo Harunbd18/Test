1d0fe9ecc0970c8b310d5bcdb7fbea8a99216339
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-07-23 16:26:50 +0600

    fixed some errors

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,7 @@ public class Calculator{
 	public static void main(String[] args){
 		
 	}
-	public int sum(int a, int b){
-		return a + b;
-	}
+	
 	public int sub(int a, int b){
 		return a - b;
 	}
