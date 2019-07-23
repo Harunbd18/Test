081ee26fb88c97c128de4e0e1c98256a874bbe35
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-07-23 16:24:09 +0600

    sub method added with two parameter

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,10 @@ public class Calculator{
 	public static void main(String[] args){
 		
 	}
+	public int sum(int a, int b){
+		return a + b;
+	}
+	public int sub(int a, int b){
+		return a - b;
+	}
 }
