6490b20a6e26ca829b747cbf940261f654ab2878
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-07-23 16:43:20 +0600

    call the sum and sub method from main method

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,9 @@
 public class Calculator{
 	public static void main(String[] args){
-		
+		int sum = sum(5,10);
+		System.out.println(sum);
+		int sub = sub(5,10);
+		System.out.println(sub);
 	}
 
 	public int sum(int a, int b){
